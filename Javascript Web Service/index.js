eb4dc7f3e204d2e300b530bf35791502c6be0209
Dimas Nazli <nazlibahaduri@gmail.com>
2022-03-08 19:58:13 +0700

    Hapi tidak bikin Hepi

--- Javascript Web Service/index.js
+++ Javascript Web Service/index.js
@@ -1,24 +1,58 @@
 const http = require('http');
  
 const requestListener = (request, response) => {
-    response.setHeader('Content-Type', 'text/html');
-    response.statusCode = 200;
-    const {method} = request;
-    if (method === 'GET'){
-        response.end('<h1>Hai!</h1>');
-    }
-    if (method === 'PUT'){
-        response.end('<h1>Arigatou!</h1>');
-    }
-    if (method === 'DELETE'){
-        response.end('<h1>Halo!</h1>');
-    }
-    if (method === 'POST'){
-        response.end('<h1>Hi!</h1>');
+    response.setHeader('Content-Type', 'application/json');
+    response.setHeader('X-Powered-By', 'NodeJS');
+ 
+    const { method, url } = request;
+ 
+    if(url === '/') {
+        if(method === 'GET') {
+            response.statusCode = 200;
+            response.end(JSON.stringify({
+                message: 'Ini adalah homepage',
+            }));
+        } else {
+            response.statusCode = 400;
+            response.end(JSON.stringify({
+                message: `Halaman tidak dapat diakses dengan ${method} request`,
+            }));
+        }
+    } else if(url === '/about') {
+        if(method === 'GET') {
+            response.statusCode = 200;
+            response.end(JSON.stringify({
+                message: 'Halo! Ini adalah halaman about',
+            }));
+        } else if(method === 'POST') {
+            let body = [];
+    
+            request.on('data', (chunk) => {
+                body.push(chunk);
+            });
+ 
+            request.on('end', () => {
+                body = Buffer.concat(body).toString();
+                const { name } = JSON.parse(body);
+                response.statusCode = 200;
+                response.end(JSON.stringify({
+                    message: `Halo, ${name}! Ini adalah halaman about`,
+                }));
+            });
+        } else {
+            response.statusCode = 400;
+            response.end(JSON.stringify({
+                message: `Halaman tidak dapat diakses menggunakan ${method}, request`
+            }));
+        }
+    } else {
+        response.statusCode = 404;
+        response.end(JSON.stringify({
+            message: 'Halaman tidak ditemukan!',
+        }));
     }
 };
- 
- 
+
 const server = http.createServer(requestListener);
  
 const port = 8000;
