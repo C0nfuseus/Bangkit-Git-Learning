5c0db175334aafe67c1ea05d6fe6fc0d3d27fac0
Dimas Nazli <nazlibahaduri@gmail.com>
2022-03-09 17:36:46 +0700

    akhirnya bisa web deploy

--- Hapi-Web-Service/routes.js
+++ Hapi-Web-Service/routes.js
@@ -34,6 +34,27 @@ const routes = [
             return 'Halaman tidak ditemukan';
         },
     },
+    {
+        method: 'GET',
+        path: '/hello/{name?}',
+        handler: (request, h) => {
+            const { name = "stranger" } = request.params;
+            const { lang } = request.query;
+     
+            if(lang === 'id') {
+                return `Hai, ${name}!`;
+            }
+            return `Hello, ${name}!`;
+        },
+    },
+    {
+        method: 'POST',
+        path: '/login',
+        handler: (request, h) => {
+        const { username, password } = request.payload;
+        return `Welcome ${username}!`;
+        },
+    }
 ];
  
 module.exports = routes;
